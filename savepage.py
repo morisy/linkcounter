48ff6e94f38864e049cfb841fed07fe7255404d3
Michael Morisy <morisy@gmail.com>
2013-04-26 00:15:27 -0400

    Commiting fix to URl

--- savepage.py
+++ savepage.py
@@ -1,11 +1,11 @@
 import urllib2
 import sys
 
-url = urllib2.urlopen("boston.com")
+url = urllib2.urlopen("http://www.boston.com")
 page = url.readlines()
 
 try:
-    with open("boston.html", "w") as file_out:
+    with open("boston.com", "w") as file_out:
         for line in page:
             file_out.write(line+"\n")
 except IOError as e:
